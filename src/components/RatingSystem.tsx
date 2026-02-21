5ef831dd33b0c00ee9f01f05719e17522f940a8c
Jhonatan <jhonatan.moraes957@gmail.com>
2026-02-21 00:03:29 -0300

    fix: remover import não utilizado bloqueando o build

--- src/components/RatingSystem.tsx
+++ src/components/RatingSystem.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { FiStar, FiSend, FiCheckCircle } from 'react-icons/fi'
 
